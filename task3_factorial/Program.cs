2d8c066d6719de91fe352a9a9162453bf4efbc14
Vlad M <vlad.mackutchev@gmail.com>
2022-04-21 21:56:31 +0300

    4 tasks

---
+++ task3_factorial/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+Console.Clear();
+Console.WriteLine("Input A");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine($"{A} -> {factorial(A)}");
+
+int factorial(int number)
+{
+    int f = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        f *= i;
+    }
+    return f;
+}
---
+++ task4_generateArray/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.WriteLine("Введи количество символов в массиве");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine(String.Join(" ", generateArray(num, 0, 2)));
+
+int[] generateArray(int numberOfSymbols, int minValue, int maxValue)
+{
+    int[] binArray = new int[numberOfSymbols];
+    for (int i = 0; i < numberOfSymbols; i++)
+    {
+        binArray[i] = getRandomNumber(minValue, maxValue);
+    }
+    return binArray;
+}
+
+int getRandomNumber(int minValue, int maxValue)
+{
+    int randomNumber = new Random().Next(minValue, maxValue);
+    return randomNumber;
+}
