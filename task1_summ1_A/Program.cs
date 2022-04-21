d4710b271e8e6f500f61880932aea6353b6b5963
Vlad M <vlad.mackutchev@gmail.com>
2022-04-21 20:56:07 +0300

    initial commit

---
+++ task1_summ1_A/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+Console.Clear();
+Console.WriteLine("Input A");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine($"{A} -> {Summa(A)}");
+
+int Summa(int number)
+{
+    int sum = 0;
+    for (int i = 0; i <= number; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
---
+++ task2_numberOfNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+Console.Clear();
+Console.WriteLine("Input A:");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine($"{A} -> {numNum(Math.Abs(A))}");
+
+int numNum(int number)
+{
+    int counter = 0;
+    while ((number) > 0)
+    {
+        counter++;
+        number /= 10;
+    }
+    return counter;
+}
